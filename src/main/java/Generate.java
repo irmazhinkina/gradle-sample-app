4e4f55c67ed6f78c519db4537e82bcc9c162e68c
irma <irmazhinkina@jetbrains.com>
2023-08-22 18:21:08 +0200

    test1

---
+++ src/main/java/Generate.java
@@ -0,0 +1,12 @@
+public class Generate {
+    int xxx = 5;
+
+    public static void main(String[] args) {
+        Generate myObj = new Generate();
+        System.out.println(myObj.xxx);
+    }
+
+    public Generate() {
+        this.xxx = xxx;
+    }
+}
